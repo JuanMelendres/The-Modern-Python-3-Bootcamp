d3afcfeb1f507d781d01bf68fcc161d399dfffec
JuanMelendres <juan.mele97@gmail.com>
2020-03-16 20:16:34 -0600

    Add types of variables
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ Variables&Strings/make_some_variables.py
@@ -0,0 +1,21 @@
+'''
+Now that we´ve learned about variables and data types, let´s get some practice.
+-Define a variable named city and set it equal to any string
+-Define a variable named price and set it equal to any float
+-Define a variable named high_score and set it equal to any int
+-Define a variable named is_having_fun and set it to a Boolean value
+You do not need to print them out, but can if you want.
+'''
+
+# Define a variable named city and set it equal to any string
+city = "Guadalajara"
+print(city)
+# Define a variable named price and set it equal to any float
+price = 19.99
+print(price)
+# Define a variable named high_score and set it equal to any int
+high_score = 100
+print(high_score)
+# Define a variable named is_having_fun and set it to a Boolean value
+is_having_fun = True
+print(is_having_fun)
