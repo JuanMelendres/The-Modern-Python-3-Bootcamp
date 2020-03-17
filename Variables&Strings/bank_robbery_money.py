37293954eb5fdf7d87bc08b5a27974421ed358fd
JuanMelendres <juan.mele97@gmail.com>
2020-03-16 19:56:41 -0600

    Add bank robbery problem
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ Variables&Strings/bank_robbery_money.py
@@ -0,0 +1,10 @@
+'''
+I just robbeb a bank with some of my friends. We got away with $19867324678987.99, 
+and now we have to split it up 5 way. Using the cash variable i´ve already 
+defined for you, print out the dollar amount each robber gets to keep.
+'''
+cash = 19867324678987.99  # DON'T CHANGE THE CASH VARIABLE
+
+# ADD YOUR CODE BELOW:
+split = cash/5
+print(split)
