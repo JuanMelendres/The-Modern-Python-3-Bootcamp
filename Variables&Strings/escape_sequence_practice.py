2bf833c88b4919ad925f97507b5d3bdd97b32611
JuanMelendres <juan.mele97@gmail.com>
2020-03-19 20:26:20 -0600

    Add Practice
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ Variables&Strings/escape_sequence_practice.py
@@ -0,0 +1,18 @@
+'''
+Get some practice with escape sequences! No need to create new variables,
+just use the existing variables we have created for you and follow the instructions.
+'''
+
+# Set the message variable equal to any string containing a new-line escape sequence
+message = "Hello\nthere"
+print(message)
+
+# Add a string to the mountains variable that when printed results in: /\/\/\
+# You will need to use an escape sequence more than once!
+mountains = "/\\/\\/\\"
+print(mountains)
+
+# Set the quotation variable to any string that contains an escaped double quotation mark
+
+quotation = "He said \"What happend\""
+print(quotation)
