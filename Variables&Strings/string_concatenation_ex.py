45f2a406f7de6b0d3b049d810387ac7eb54571d1
JuanMelendres <juan.mele97@gmail.com>
2020-04-03 18:12:11 -0600

    Add string concatenation example
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ Variables&Strings/string_concatenation_ex.py
@@ -0,0 +1,10 @@
+'''
+Set the variable called greeting to some greeting, e.g. "Hello".
+Set the variable called name to some name, e.g. "Heisenberg".
+Then set the variable called greet_name so that it concatenates greeting, name and a space " " between them.
+'''
+
+greeting = "Hello"
+name = "Juan"
+greet_name = greeting + " " + name
+print(greet_name)
