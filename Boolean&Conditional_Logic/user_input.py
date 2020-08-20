e54f2e681313942fee36225ca94d8655f9cce47a
JuanMelendres <juan.mele97@gmail.com>
2020-08-20 18:20:46 -0500

    add Example of user input
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ Boolean&Conditional_Logic/user_input.py
@@ -0,0 +1,9 @@
+name = input("Enter tour name here: ")
+print("Your name is: " + name)
+
+# Two ways to do it
+print("Another way")
+
+print("Enter tour name here: ")
+name = input()
+print("Your name is: " + name)
