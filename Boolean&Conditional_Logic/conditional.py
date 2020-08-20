573e88f9133a8be3e84fbe008bbd8853279ae337
JuanMelendres <juan.mele97@gmail.com>
2020-08-20 18:21:18 -0500

    Add Example of conditional
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ Boolean&Conditional_Logic/conditional.py
@@ -0,0 +1,9 @@
+# name = input("Enter tour name here: ")
+name = "Jon Snow"
+
+if name == "Arya stark":
+    print("Valar Morghulis")
+elif name == "Jon Snow":
+    print("You know nothing")
+else:
+    print("Carry on")
