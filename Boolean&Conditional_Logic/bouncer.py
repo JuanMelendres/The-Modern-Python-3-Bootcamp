d4ef946f608404271f76afc8ffb3dfd11ffe5832
JuanMelendres <juan.mele97@gmail.com>
2020-08-24 14:40:28 -0500

    Update if else statement
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- Boolean&Conditional_Logic/bouncer.py
+++ Boolean&Conditional_Logic/bouncer.py
@@ -1,15 +1,26 @@
 # ask for age
 age = input("How old are you: ")
 
-if age != "":
-    if int(age) >= 18 and int(age) < 21:
+#if age:
+#   if int(age) >= 18 and int(age) < 21:
         # 18-21 wristband
-        print("You can enter, but need a wristband!")
-    elif int(age) >= 21:
+#        print("You can enter, but need a wristband!")
+#    elif int(age) >= 21:
         # 21+ drink, normal entry
-        print("You are good to enter and can drink!")
-    else:
+#        print("You are good to enter and can drink!")
+#    else:
         # too young, sorry
+#        print("Yoy can´t come in, little one! :(")
+#else:
+#    print("Please enter an age!")
+
+if age:
+    age = int(age)
+    if age >= 21:
+        print("You are good to enter and can drink!")
+    elif age >= 18:
+        print("You can enter, but need a wristband!")
+    else:
         print("Yoy can´t come in, little one! :(")
 else:
     print("Please enter an age!")
