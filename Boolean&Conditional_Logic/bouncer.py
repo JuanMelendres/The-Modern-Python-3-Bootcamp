e9e0b5b9141ab8a1de598cc7cc887514a6ac74a7
JuanMelendres <juan.mele97@gmail.com>
2020-08-24 07:07:24 -0500

    Add else if example
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ Boolean&Conditional_Logic/bouncer.py
@@ -0,0 +1,15 @@
+# ask for age
+age = input("How old are you: ")
+
+if age != "":
+    if int(age) >= 18 and int(age) < 21:
+        # 18-21 wristband
+        print("You can enter, but need a wristband!")
+    elif int(age) >= 21:
+        # 21+ drink, normal entry
+        print("You are good to enter and can drink!")
+    else:
+        # too young, sorry
+        print("Yoy can´t come in, little one! :(")
+else:
+    print("Please enter an age!")
