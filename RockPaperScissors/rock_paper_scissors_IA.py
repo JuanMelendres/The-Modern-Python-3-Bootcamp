7655b839c9c126f90f38c02f6d0e90ad594d2614
JuanMelendres <juan.mele97@gmail.com>
2020-08-26 11:22:16 -0500

    Add rock paper scissors game version IA
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ RockPaperScissors/rock_paper_scissors_IA.py
@@ -0,0 +1,40 @@
+from random import randint
+print("Rock...")
+print("Paper...")
+print("Scissors...")
+
+player = input("Player 1, make your move: ").lower()
+
+rand_num = randint(0,2)
+
+if rand_num == 0:
+    computer = "rock"
+elif rand_num == 1:
+    computer = "paper"
+else:
+    computer = "scissors"
+
+print(f"IA plays {computer}")
+
+if player:
+    if player == computer:
+        print("It`s a tie!")
+    elif player == "rock":
+        if computer == "scissors":
+            print("Player1 wins!")
+        else:
+            print("IA wins!")
+    elif player == "paper":
+        if computer == "rock":
+            print("Player1 wins!")
+        else:
+            print("IA wins!")
+    elif player == "scissors":
+        if computer == "paper":
+            print("Player1 wins!")
+        else:
+            print("IA wins!")
+    else:
+        print("something went wrong")
+else:
+    print("something went wrong")
