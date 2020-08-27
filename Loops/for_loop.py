3de06c6b84c45f7b1c65007a50c3da5546c41db9
JuanMelendres <juan.mele97@gmail.com>
2020-08-27 07:13:23 -0500

    Add example for looop
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ Loops/for_loop.py
@@ -0,0 +1,13 @@
+# Loops with ranges (for) int
+for number in range(10):
+    print(number)
+
+# Range skiping 2 numbers
+for number in range(1, 10, 2):
+    print(number)
+
+# Loops with ranges (for) strings
+for letter in "coffee":
+    print(letter)
+
+ 
