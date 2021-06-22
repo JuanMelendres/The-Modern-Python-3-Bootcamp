e3cdc8cf311d3459b559fea01f5e209f03d08ead
JuanMelendres <juan.mele97@gmail.com>
2021-06-22 14:39:09 -0500

    Add list basic in python
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ List/list.py
@@ -0,0 +1,24 @@
+demo_list = ["Install Python", "Learn Python", True, 17]
+
+# Print the list
+print(demo_list)
+# Print the length of the list
+print(len(demo_list))
+
+nums = list(range(1, 100))
+print(nums)
+
+colors = ["purple", "teal", "orange"]
+print(colors)
+
+the_best = colors[0]
+print(the_best)
+
+# It goes backwards with negative numbers
+print(colors[-1])
+
+print("purple" in colors)
+print("Purple" in colors)
+
+if "teal" in colors:
+    print("YOU HAVE GOOD TASTE IN COLORS!")
