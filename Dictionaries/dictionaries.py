4c1a706abbde75ee13c272151b2f7582e36bfd10
JuanMelendres <juan.mele97@gmail.com>
2021-06-29 13:38:05 -0500

    Add dictionaries
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ Dictionaries/dictionaries.py
@@ -0,0 +1,34 @@
+# Making a Dictionary
+
+instructor = {
+    "name": "Colt",
+    "owns_dog": True,
+    "nums_courses": 4,
+    "favorite_language": "Python",
+    "is_hilarious": False,
+    44: "My favorite number!"
+}
+
+print(instructor)
+
+cat = {
+    "name": "Blue",
+    "age": 3.5,
+    "isCute": True
+}
+
+print(cat)
+
+# Dict function
+
+cat2 = dict(color = "Brown", age = 0.5)
+
+print(cat2)
+
+# Accessing individual values
+
+print(cat["name"])
+
+property = "isCute"
+
+print(cat[property])
