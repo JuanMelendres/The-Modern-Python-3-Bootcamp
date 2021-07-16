110ee418d01f73a6525435986b3158a4ca65cab3
JuanMelendres <juan.mele97@gmail.com>
2021-07-16 10:23:35 -0500

    Add All solutions to the exercises
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- Functions/exercises.py
+++ Functions/exercises.py
@@ -91,4 +91,155 @@ multiple_letter_count("awesome") # {'a': 1, 'e': 2, 'm': 1, 'o': 1, 's': 1, 'w':
 def multiple_letter_count(str):
     return {char: str.count(char) for char in str}
 
-print(multiple_letter_count("awesome"))
+# print(multiple_letter_count("awesome"))
+
+'''
+list_manipulation([1,2,3], "remove", "end") # 3
+list_manipulation([1,2,3], "remove", "beginning") #  1
+list_manipulation([1,2,3], "add", "beginning", 20) #  [20,1,2,3]
+list_manipulation([1,2,3], "add", "end", 30) #  [1,2,3,30]
+'''
+
+def list_manipulation(list, command, location, value=None):
+    if value:
+        if location == "end":
+            list.append(value)
+            return list
+        else:
+            list.insert(0, value)
+            return list
+    else:
+        if location == "end":
+            return list.pop()
+        else:
+            return list.pop(0)
+
+# print(list_manipulation([1,2,3], "add", "beginning", 20))
+
+# Another Solution
+
+def list_manipulation(collection, command, location, value=None):
+    if(command == "remove" and location == "end"):
+        return collection.pop()
+    elif(command == "remove" and location == "beginning"):
+        return collection.pop(0)
+    elif(command == "add" and location == "beginning"):
+        collection.insert(0,value)
+        return collection
+    elif(command == "add" and location == "end"):
+        collection.append(value)
+        return collection
+
+'''
+is_palindrome('testing') # False
+is_palindrome('tacocat') # True
+is_palindrome('hannah') # True
+is_palindrome('robert') # False
+is_palindrome('amanaplanacanalpanama') # True
+'''
+
+def is_palindrome(str):
+    return str == str[::-1]
+
+# Another Solution
+
+def is_palindrome(string):
+    stripped = string.replace(" ", "")
+    return stripped == stripped[::-1]
+
+# print(is_palindrome('testing'))
+
+'''
+frequency([1,2,3,4,4,4], 4) # 3
+frequency([True, False, True, True], False) # 1
+'''
+
+def frequency(list, search_term):
+    return list.count(search_term)
+
+# print([1,2,3,4,4,4], 4)
+
+'''
+multiply_even_numbers([2,3,4,5,6]) # 48
+'''
+
+def multiply_even_numbers(lst):
+    total = 1
+    for val in lst:
+        if val % 2 == 0:
+            total = total * val
+    return total
+
+# print(multiply_even_numbers([2,3,4,5,6]))
+
+'''
+capitalize("tim") # "Tim"
+capitalize("matt") # "Matt"
+'''
+
+def capitalize(str):
+    return str[:1].upper() + str[1:]
+
+# print(capitalize("tim"))
+
+'''
+compact([0,1,2,"",[], False, {}, None, "All done"]) # [1,2, "All done"]
+'''
+
+def compact(lst):
+    return [value for value in lst if value]
+
+#  Another solution
+def compact(l):
+    truthy_vals = []
+    for val in l:
+        if val: truthy_vals.append(val)
+    return truthy_vals
+
+# print(compact([0,1,2,"",[], False, {}, None, "All done"]))
+
+# flesh out intersection pleaseeeee
+def intersection(a, b):
+    return [val for val in a if val in b]
+
+# Another Solutions
+def intersection(list1, list2):
+    return [val for val in set(list1) & set(list2)]
+
+def intersection(l1, l2):
+    in_common = []
+    for val in l1:
+        if val in l2:
+            in_common.append(val)
+    return in_common
+
+# print(intersection([1,2,3], [2,3,4]))
+
+'''
+def isEven(num):
+    return num % 2 == 0
+
+partition([1,2,3,4], isEven) # [[2,4],[1,3]]
+'''
+
+def isEven(num):
+    return num % 2 == 0
+
+def partition(list, fn=isEven):
+    truthy_list = []
+    falsy_list = []
+    for num in list:
+        if fn(num):
+            truthy_list.append(num)
+        else:
+            falsy_list.append(num)
+    return [truthy_list, falsy_list]
+
+# Another Solutions 
+def partition(lst, fn):
+    return [[val for val in lst if fn(val)], [val for val in lst if not fn(val)]]
+
+def partition(l, callback):
+    return [[l.pop(l.index(i)) for i in l if callback(i)],l]
+
+# print(partition([1,2,3,4], isEven))
